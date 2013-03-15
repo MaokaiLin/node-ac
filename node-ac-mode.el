826cb6aa727211960aff54b1185f1c25aafc2740
Maokai Lin <MaokaiLin@users.noreply.github.com>
2013-03-15 05:04:00 -0400

    Slight change of markdown formatting, and added github URL to code comments.

--- node-ac-mode.el
+++ node-ac-mode.el
@@ -1,11 +1,11 @@
 ;; node-ac.el --- Auto-complete for Node.js in Emacs
 
-;; Copyright (C) 2012 Maokai Lin
+;; Copyright (C) 2013 Maokai Lin
 
 ;; Version: 0.1
 ;; Keywords: Node.js Emacs Autocomplete
 ;; Author: Maokai Lin <Maokai.Lin@gmail.com>
-;; URL: 
+;; URL: https://github.com/MaokaiLin/node-ac
 
 ;; This file is not part of GNU Emacs.
 
